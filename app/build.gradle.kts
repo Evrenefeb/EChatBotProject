df1951d2710ed7f34c71af2fc640773d52c8f7f5
Evrenefeb <141721390+Evrenefeb@users.noreply.github.com>
2025-05-03 18:27:32 +0300

    Added Firebase to System
    Added google-services and Sync'ed with the IDE and System itself

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,6 @@
 plugins {
+    id("com.google.gms.google-services")
+
     alias(libs.plugins.android.application)
 }
 
@@ -40,4 +42,23 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    // Import the Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:33.13.0"))
+
+    // TODO: Add the dependencies for Firebase products you want to use
+    // When using the BoM, don't specify versions in Firebase dependencies
+    implementation("com.google.firebase:firebase-analytics")
+
+    // Add the dependencies for any other desired Firebase products
+    // https://firebase.google.com/docs/android/setup#available-libraries
+
+    // Firebase Authentication
+    implementation("com.google.firebase:firebase-auth:23.2.0") // Use the latest version
+    // Latest : 23.2.0
+
+    // Firebase Firestore
+    implementation("com.google.firebase:firebase-firestore:25.1.4") // Use the latest version
+    // Latest : 25.1.4
+
 }
