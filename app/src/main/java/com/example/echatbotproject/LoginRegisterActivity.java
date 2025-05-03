3f8c51680e360d45ab77cd513e2b0722c48fc3ae
Evrenefeb <141721390+Evrenefeb@users.noreply.github.com>
2025-05-03 20:00:01 +0300

    Update LoginRegisterActivity
    Added Registration and Logging in logic to System using Firebase API

--- app/src/main/java/com/example/echatbotproject/LoginRegisterActivity.java
+++ app/src/main/java/com/example/echatbotproject/LoginRegisterActivity.java
@@ -6,11 +6,19 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 
 import android.content.Intent;
+import android.widget.Toast;
+
+// Importing firebase auth
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Objects;
 
 public class LoginRegisterActivity extends AppCompatActivity{
 
@@ -20,6 +28,7 @@ public class LoginRegisterActivity extends AppCompatActivity{
     private Button buttonLogin;
     private Button buttonRegister;
     private ProgressBar progressBar;
+    private FirebaseAuth mAuth;
 
     // Overriden Methods
 
@@ -33,6 +42,7 @@ public class LoginRegisterActivity extends AppCompatActivity{
         buttonLogin = findViewById(R.id.buttonLogin);
         buttonRegister = findViewById(R.id.buttonRegister);
         progressBar = findViewById(R.id.progressBar);
+        mAuth = FirebaseAuth.getInstance();
 
         // Initialize Event Listeners
         buttonLogin.setOnClickListener(v -> {
@@ -47,6 +57,58 @@ public class LoginRegisterActivity extends AppCompatActivity{
     // Event Listeners
 
     private void loginOnClickEvent(){
+
+        String username = editTextUsername.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        if (TextUtils.isEmpty(username)) {
+            editTextUsername.setError("Email is required.");
+            return;
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            editTextPassword.setError("Password is required.");
+            return;
+        }
+
+        progressBar.setVisibility(View.VISIBLE);
+
+        mAuth.signInWithEmailAndPassword(username, password).addOnCompleteListener(
+                this,
+                task -> {
+                    // Hide progress bar
+                    progressBar.setVisibility(View.GONE);
+
+
+                    // Check : If login successful
+                    if(task.isSuccessful()){
+                        Toast.makeText(
+                                LoginRegisterActivity.this,
+                                "Login Successful",
+                                Toast.LENGTH_SHORT
+                        ).show();
+
+                        Intent intent = new Intent(LoginRegisterActivity.this, ChatbotActivity.class);
+                        startActivity(intent);
+                        finish();
+
+                    } else{
+
+                        Toast.makeText(
+                                LoginRegisterActivity.this,
+                                "Login failed: " +  Objects.requireNonNull(task.getException()).getMessage(),
+                                Toast.LENGTH_LONG
+                        ).show();
+
+                    }
+
+
+                }
+        );
+
+
+
+
         // TODO: Handle login logic
         // ...
         // If login is successful:
@@ -56,12 +118,62 @@ public class LoginRegisterActivity extends AppCompatActivity{
     }
 
     private void registerOnClickEvent(){
-        // TODO: Handle registration logic here
-        // ...
-        // If registration is successful:
-        Intent intent = new Intent(LoginRegisterActivity.this, ChatbotActivity.class);
-        startActivity(intent);
-        finish();
+
+        String username = editTextUsername.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        // Check : If username field empty
+        if(TextUtils.isEmpty(username)){
+            editTextUsername.setError("Username is required.");
+            return;
+        }
+
+        // Check : If password field empty
+        if(TextUtils.isEmpty(password)){
+            editTextPassword.setError("Password is required.");
+            return;
+        }
+
+
+        progressBar.setVisibility(View.VISIBLE);
+
+        mAuth.createUserWithEmailAndPassword(username, password).addOnCompleteListener(
+                    this,
+                    task -> {
+                        // Hide progressbar
+                        progressBar.setVisibility(View.GONE);
+
+                        // Check : If Registration is successful
+                        if(task.isSuccessful()){
+                            Toast.makeText(LoginRegisterActivity.this,
+                                    "Registration is successful",
+                                    Toast.LENGTH_SHORT
+                            ).show();
+
+
+                            Intent intent = new Intent(LoginRegisterActivity.this, ChatbotActivity.class);
+                            startActivity(intent);
+                            finish();
+
+
+                        } else {
+
+                            Toast.makeText(
+                                    LoginRegisterActivity.this,
+                                    "Registration Failed.",
+                                    Toast.LENGTH_LONG
+                            ).show();
+
+                        }
+
+
+
+
+
+                    }
+                );
+
+
     }
 
     // TODO: Implement Methods for handling login and registration
